a06cd48ce6dadf7f7d523bb15ca2dfa356ea7043
David Mold <davidmold@gmail.com>
2025-05-03 20:59:57 -0400

    Added some things

--- init.lua
+++ init.lua
@@ -151,6 +151,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*.vue",
+  callback = function()
+    vim.defer_fn(function()
+      vim.treesitter.start()
+    end, 20)
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
